3085c8e84618447b8d56ba467014ae6d273c3f06
Joe Fitzgerald <jfitzgerald@pivotal.io>
2018-03-07 16:24:35 -0700

    Get port from environment when running on CF

--- cmd/ignition/main.go
+++ cmd/ignition/main.go
@@ -4,11 +4,22 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/cloudfoundry-community/go-cfenv"
 	"github.com/pivotalservices/ignition/http"
 )
 
 func main() {
-	serveURI := fmt.Sprintf(":%v", 3000)
+	var serveURI string
+	if cfenv.IsRunningOnCF() {
+		env, err := cfenv.Current()
+		if err != nil {
+			log.Fatal(err)
+		}
+		serveURI = fmt.Sprintf(":%v", env.Port)
+	} else {
+		serveURI = fmt.Sprintf(":%v", 3000)
+	}
+
 	fmt.Println(fmt.Sprintf("Starting Server listening on %s", serveURI))
 	log.Fatal(http.Run(serveURI))
 }
