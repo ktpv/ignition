86c43c8e939a2e39cae97431adfc4e812e44a66c
Zac Bergquist <zbergquist99@gmail.com>
2018-04-06 14:47:26 -0600

    Refactor speech bubble styles
    Reduce duplication by extracting the logic into a single function that accepts foreground/background colors.

--- web/src/components/body.js
+++ web/src/components/body.js
@@ -15,6 +15,35 @@ import step1 from './../../images/step_1.svg'
 import step2 from './../../images/step_2.svg'
 import step3 from './../../images/step_3.svg'
 
+const makeSpeechBubbleClass = (bgColor, fgColor) => ({
+  position: 'relative', // so we can overlap the button
+
+  padding: '24px',
+  borderRadius: '15px',
+  backgroundColor: bgColor,
+  color: fgColor,
+
+  fontSize: '1.75rem',
+  height: 'auto',
+  width: '40vw',
+
+  '&:before': {
+    content: '""',
+    width: '0px',
+    height: '0px',
+    position: 'absolute',
+    borderLeft: `100px solid ${bgColor}`,
+    borderRight: '100px solid transparent',
+    borderTop: `25px solid ${bgColor}`,
+    borderBottom: '25px solid transparent',
+    right: '-175px',
+    top: '75px'
+  }
+})
+
+const speech1Background = '#083c61'
+const speech2Background = '#9bd2d2'
+
 const styles = theme => ({
   body: {
     fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
@@ -28,24 +57,8 @@ const styles = theme => ({
     position: 'absolute',
     bottom: -3 * theme.spacing.unit
   },
-  speechBubble: {
-    position: 'relative', // so we can overlap the button
-
-    padding: '24px',
-    borderRadius: '15px',
-    '&:before': {
-      content: '""',
-      width: '0px',
-      height: '0px',
-      position: 'absolute',
-      borderLeft: '100px solid #083c61',
-      borderRight: '100px solid transparent',
-      borderTop: '25px solid #083c61',
-      borderBottom: '25px solid transparent',
-      right: '-175px',
-      top: '75px'
-    }
-  },
+  welcomeSpeech: makeSpeechBubbleClass(speech1Background, 'white'),
+  spacesSpeech: makeSpeechBubbleClass(speech2Background, 'black'),
 
   // CTA 1: Welcome
   ctaWelcome: {
@@ -60,14 +73,6 @@ const styles = theme => ({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center'
-  },
-  welcomeSpeech: {
-    color: 'white',
-    fontSize: '1.75rem',
-    height: 'auto',
-    width: '40vw',
-    borderRadius: '15px',
-    backgroundColor: '#083c61'
   },
   rocketMan: {
     backgroundImage: `url("${rocketMan}")`,
@@ -125,13 +130,6 @@ const styles = theme => ({
     flexDirection: 'row',
     justifyContent: 'space-around'
   },
-  spacesSpeech: {
-    color: 'black',
-    fontSize: '2rem',
-    height: '375px',
-    width: '40vw',
-    backgroundColor: '#9ed4d4'
-  },
   moonMan: {
     backgroundImage: `url("${moonMan}")`,
     height: '375px',
@@ -150,20 +148,23 @@ class Body extends React.Component {
 
   handleOrgButtonClick = () => {
     // Spinner
-    window.fetch('/organization', {
-      credentials: 'same-origin'
-    }).then(response => {
-      if (!response.ok) {
-        return
-      }
-      return response.json()
-    }).then(response => {
-      if (!response) {
-        return
-      }
-      this.setState({orgUrl: response.url})
-      window.location = response.url
-    })
+    window
+      .fetch('/organization', {
+        credentials: 'same-origin'
+      })
+      .then(response => {
+        if (!response.ok) {
+          return
+        }
+        return response.json()
+      })
+      .then(response => {
+        if (!response) {
+          return
+        }
+        this.setState({ orgUrl: response.url })
+        window.location = response.url
+      })
   }
 
   renderWelcomeInfo () {
@@ -172,13 +173,10 @@ class Body extends React.Component {
     return (
       <div className={classes.ctaWelcome}>
         <div>
-          <div className={classes.welcomeSpeech + ' ' + classes.speechBubble}>
-            {
-              introMessages.map(msg => <p>{msg}</p>)
-            }
-            {
-              false && this.renderButton('Give Me an Org!', classes.speechButton)
-            }
+          <div className={classes.welcomeSpeech}>
+            {introMessages.map((msg, i) => <p key={i}>{msg}</p>)}
+            {false &&
+              this.renderButton('Give Me an Org!', classes.speechButton)}
           </div>
         </div>
         <div className={classes.rocketMan} />
@@ -192,16 +190,31 @@ class Body extends React.Component {
       <div className={classes.ctaSteps}>
         <div className={classes.pewPew}>
           <div className={classes.step}>
-            <div><img className={classes.stepImage} src={step1} /></div>
-            Get the <a href='https://docs.pivotal.io/pivotalcf/latest/cf-cli/'>Cloud Foundry CLI from Pivotal.</a>
+            <div>
+              <img className={classes.stepImage} src={step1} />
+            </div>
+            Get the{' '}
+            <a href="https://docs.pivotal.io/pivotalcf/latest/cf-cli/">
+              Cloud Foundry CLI from Pivotal.
+            </a>
           </div>
           <div className={classes.step}>
-            <div><img className={classes.stepImage} src={step2} /></div>
-            Download the <a href='https://github.com/cloudfoundry-samples/spring-music'>sample app from Github.</a>
+            <div>
+              <img className={classes.stepImage} src={step2} />
+            </div>
+            Download the{' '}
+            <a href="https://github.com/cloudfoundry-samples/spring-music">
+              sample app from Github.
+            </a>
           </div>
           <div className={classes.step}>
-            <div><img className={classes.stepImage} src={step3} /></div>
-            Learn to <a href='https://docs.pivotal.io/pivotalcf/latest/devguide/deploy-apps/deploy-app.html'>deploy an app.</a>
+            <div>
+              <img className={classes.stepImage} src={step3} />
+            </div>
+            Learn to{' '}
+            <a href="https://docs.pivotal.io/pivotalcf/latest/devguide/deploy-apps/deploy-app.html">
+              deploy an app.
+            </a>
           </div>
         </div>
       </div>
@@ -212,10 +225,8 @@ class Body extends React.Component {
     const { classes } = this.props
     return (
       <div className={classes.ctaSpaces}>
-        <div className={classes.spacesSpeech + ' ' + classes.speechBubble}>
-          {
-            spaceMessages.map(msg => <p>{msg}</p>)
-          }
+        <div className={classes.spacesSpeech}>
+          {spaceMessages.map((msg, i) => <p key={i}>{msg}</p>)}
         </div>
         <div className={classes.moonMan} />
       </div>
@@ -227,9 +238,10 @@ class Body extends React.Component {
     if (extraClasses) classes += ' ' + extraClasses
     return (
       <Button
-        variant='raised'
+        variant="raised"
         className={classes}
-        onClick={this.handleOrgButtonClick}>
+        onClick={this.handleOrgButtonClick}
+      >
         {text}
       </Button>
     )
